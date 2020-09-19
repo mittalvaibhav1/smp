2838983ff5f1f2ab1cbb914227ba4faad7ae18b1
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-09-19 11:22:43 +0530

    Setup Firebase

---
+++ smp/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAJ5EtD3O-fD-cWxyy4-U95tak05uWhHKk",
+    authDomain: "mysmp-5c477.firebaseapp.com",
+    databaseURL: "https://mysmp-5c477.firebaseio.com",
+    projectId: "mysmp-5c477",
+    storageBucket: "mysmp-5c477.appspot.com",
+    messagingSenderId: "131297889429",
+    appId: "1:131297889429:web:753813d37a9403fe2c1f9c",
+    measurementId: "G-NJC2WBBCP7"
+  };
